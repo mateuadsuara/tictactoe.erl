d94b815a79d0a0495bef79a667f7b18fb68f21cb
Mateu Adsuara <mateuadsuara@gmail.com>
2015-10-03 19:16:19 +0100

    Removes unnecessary text_ui

--- src/cli.erl
+++ src/cli.erl
@@ -13,7 +13,7 @@ display_last_move(Moves) ->
   io:format("Moved to ~w.~n", [lists:last(Moves)]).
 
 display_board(Board) ->
-  io:format("~n~s~n", [text_ui:format(Board)]).
+  io:format("~n~s~n", [format(Board)]).
 
 display_status({ongoing, ActivePlayer}) ->
   io:format("~p is playing now...~n", [ActivePlayer]);
@@ -21,3 +21,35 @@ display_status({finished, draw}) ->
   io:format("It is a draw.~n");
 display_status({finished, Winner}) ->
   io:format("~p has won!~n", [Winner]).
+
+format(Board) ->
+  Cells = format_cells(Board),
+  Rows = format_rows(square_rows(Cells)),
+  add_separators(Rows).
+
+add_separators(Rows) ->
+  CellsPerRow = length(Rows),
+  Separator = string:join(
+                lists:duplicate(CellsPerRow, "---"),
+                "+"),
+  string:join(Rows, Separator ++ "\n").
+
+format_rows(Rows) ->
+  lists:map(fun format_row/1, Rows).
+
+format_row(Row) ->
+  string:join(Row, "|") ++ "\n".
+
+square_rows(Cells) ->
+  SideSize = round(math:sqrt(length(Cells))),
+  list:group_each(SideSize, Cells).
+
+format_cells(Board) ->
+  BoardTuples = list:with_indexes(Board),
+  lists:map(fun to_cell/1, BoardTuples).
+
+to_cell(BoardTuple) ->
+  " " ++ to_string(BoardTuple) ++ " ".
+
+to_string({Space, empty}) -> integer_to_list(Space);
+to_string({_, Mark}) -> atom_to_list(Mark).
