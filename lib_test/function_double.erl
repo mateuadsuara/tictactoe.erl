3f11050e3e5eebacbb3523ade2f7eedf832c2ecc
Mateu Adsuara <mateuadsuara@gmail.com>
2015-10-03 19:15:10 +0100

    Simplifies function_double

--- lib_test/function_double.erl
+++ lib_test/function_double.erl
@@ -1,38 +1,35 @@
 -module(function_double).
 
--export([new/1, arity1/1, arity2/1, arity3/1, destroy_and_get_arguments/1]).
+-export([new/2]).
 
-new(ReturnValues) ->
-  StubState = mutable_state:new(ReturnValues),
-  SpyState = mutable_state:new([]),
-  {StubState, SpyState}.
+new(Arity, ReturnValues) ->
+  State = spawn(fun() -> state(ReturnValues, []) end),
+  Spy = case Arity of
+          0 -> fun() -> call(State, []) end;
+          1 -> fun(Arg) -> call(State, [Arg]) end
+        end,
+  GetArguments = fun() -> arguments(State) end,
+  {Spy, GetArguments}.
 
-arity1(States) ->
-  fun(Arg) ->
-    pop_and_store(States, [Arg])
+
+call(State, Arguments) ->
+  State ! {call, self(), Arguments},
+  receive
+    ReturnValue -> ReturnValue
   end.
 
-arity2(States) ->
-  fun(Arg1, Arg2) ->
-    pop_and_store(States, [Arg1, Arg2])
+arguments(State) ->
+  State ! {get, self()},
+  receive
+    Arguments -> Arguments
   end.
 
-arity3(States) ->
-  fun(Arg1, Arg2, Arg3) ->
-    pop_and_store(States, [Arg1, Arg2, Arg3])
+state(ReturnValues, ArgumentsReceived) ->
+  receive
+    {call, From, Arguments} ->
+      [ReturnValue|Rest] = ReturnValues,
+      From ! ReturnValue,
+      state(Rest, ArgumentsReceived ++ [Arguments]);
+    {get, From} ->
+      From ! ArgumentsReceived
   end.
-
-pop_and_store({StubState, SpyState}, Args) ->
-  mutable_state:swap(
-    fun(PassedArgs) -> PassedArgs ++ [Args] end,
-    SpyState
-  ),
-  [Head|_] = mutable_state:swap(
-               fun([_|Tail]) -> Tail end,
-               StubState
-             ),
-  Head.
-
-destroy_and_get_arguments({StubState, SpyState}) ->
-  mutable_state:destroy(StubState),
-  mutable_state:destroy(SpyState).
