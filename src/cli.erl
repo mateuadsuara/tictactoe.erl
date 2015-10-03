363c1c1d1d0744e2ebd32065c94794512d3ab103
Mateu Adsuara <mateuadsuara@gmail.com>
2015-10-03 15:02:01 +0100

    Adds Cli

---
+++ src/cli.erl
@@ -0,0 +1,18 @@
+-module(cli).
+
+-export([update/1]).
+
+update(Game) ->
+  display_board(game:board(Game)),
+  display_status(game:status(Game)).
+
+
+display_board(Board) ->
+  io:format(text_ui:format(Board)).
+
+display_status({ongoing, ActivePlayer}) ->
+  io:format("~p is playing now...~n", [ActivePlayer]);
+display_status({finished, draw}) ->
+  io:format("It is a draw.~n");
+display_status({finished, Winner}) ->
+  io:format("~p has won!~n", [Winner]).
