f5b2cb02febcef24d706933059d3773a08acae29
Mateu Adsuara <mateuadsuara@gmail.com>
2015-10-03 11:49:52 +0100

    Improves hugely the usability of test_io

--- lib_test/test_io.erl
+++ lib_test/test_io.erl
@@ -1,34 +1,100 @@
 -module(test_io).
--export([output_when/1]).
+-export([spy/1, redirect/2]).
 
-output_when(Function) ->
-  Messages = messages_to_group_leader_during(Function),
-  Strings = lists:map(fun to_string/1, Messages),
-  string:join(Strings, "").
-
-messages_to_group_leader_during(Function) ->
+spy(Function) ->
   Original = erlang:group_leader(),
-  Spy = fun Self(ReceivedMessagesState) ->
-    receive
-      {get_spied_messages, Pid} ->
-        Pid ! {messages, ReceivedMessagesState};
-      Message ->
-        Original ! Message,
-        Self(lists:append(ReceivedMessagesState, [Message]))
-    end
-  end,
-  Surrogate = spawn(fun() -> Spy([]) end),
+  Surrogate = spawn(fun() -> spy_actor(Original, []) end),
   erlang:group_leader(Surrogate, self()),
   Function(),
   erlang:group_leader(Original, self()),
   Surrogate ! {get_spied_messages, self()},
-  receive {messages, M} -> M end.
+  receive
+    {messages, Messages} ->
+      Messages
+  end.
 
-to_string(Message) ->
-  case Message of
-    {io_request, _, _,
+redirect(InputString, Function) ->
+  Original = erlang:group_leader(),
+  Surrogate = spawn(fun() ->
+                        surrogate(
+                          Original,
+                          [],
+                          InputString)
+                    end),
+  erlang:group_leader(Surrogate, self()),
+  Result = Function(),
+  erlang:group_leader(Original, self()),
+  Surrogate ! {get_output, self()},
+  Output = receive {output, O} -> O end,
+  {Result, Output}.
+
+
+spy_actor(GroupLeader, Messages) ->
+  receive
+    {get_spied_messages, Requester} ->
+      Requester ! {messages, Messages};
+    {record_message, Message} ->
+      spy_actor(
+        GroupLeader,
+        lists:append(Messages, [Message]));
+    {io_request, From, Ref, Op} ->
+      Request = {io_request, From, Ref, Op},
+      Self = self(),
+      Self ! {record_message, Request},
+      ResponseSpy = spawn(fun RSpy() ->
+            receive
+              Response ->
+                Self ! {record_message, Response},
+                From ! Response,
+                RSpy()
+            end
+      end),
+      RedirectedRequest = {io_request, ResponseSpy, Ref, Op},
+      GroupLeader ! RedirectedRequest,
+      spy_actor(GroupLeader, Messages);
+    UnknownMessage ->
+      error_logger:info_msg("Unhandled message: ~p", [UnknownMessage]),
+      spy_actor(GroupLeader, Messages)
+    end.
+
+surrogate(GroupLeader, OutputString, InputString) ->
+  receive
+    {get_output, Requester} ->
+      Requester ! {output, OutputString};
+    {io_request, From, Ref,
+     {get_until, unicode, [], io_lib, fread, [Format]}} ->
+      case io_lib:fread(Format, InputString) of
+        {ok, Result, RemainingInputString} ->
+          From ! {io_reply, Ref, {ok, Result}},
+          surrogate(GroupLeader, OutputString, RemainingInputString);
+        {error, What} ->
+          From ! {io_reply, Ref, {error, What}},
+          surrogate(GroupLeader, OutputString, remove_line(InputString));
+        {more, _, _, _} ->
+          From ! {io_reply, Ref, eof},
+          surrogate(GroupLeader, OutputString, "")
+      end;
+    {io_request, From, Ref,
      {put_chars, Encoding, Module, Func, Args}} ->
-      unicode:characters_to_list(
+      Request = {io_request, From, Ref,
+                 {put_chars, Encoding, Module, Func, Args}},
+      GroupLeader ! Request,
+      Output = unicode:characters_to_list(
         apply(Module, Func, Args),
-        Encoding)
+        Encoding),
+      ExtendedOutput = OutputString ++ Output,
+      surrogate(GroupLeader, ExtendedOutput, InputString);
+    Message ->
+      GroupLeader ! Message,
+      surrogate(GroupLeader, OutputString, InputString)
+  end.
+
+remove_line(String) ->
+  case string:str(String, "\n") of
+    0 -> "";
+    1 -> remove_line(string:substr(String, 2));
+    NextLineIndex ->
+      string:substr(
+        String,
+        NextLineIndex + string:len("\n"))
   end.
--- test/test_io_tests.erl
+++ test/test_io_tests.erl
@@ -1,15 +1,79 @@
 -module(test_io_tests).
 -include_lib("eunit/include/eunit.hrl").
 
-captures_several_outputs_test() ->
-  "foobar" =
-    test_io:output_when(fun() ->
-                            io:format("foo"),
-                            io:format("bar")
-                        end).
+spies_the_io_messages_test() ->
+  [{io_request,_,Ref,
+    {put_chars,unicode,io_lib,format,["foo",[]]}},
+   {io_reply,Ref,ok}] =
+    test_io:spy(fun() -> ok = io:format("foo") end).
 
-captures_complex_formatting_test() ->
-  "foo\"bar\"\n" =
-    test_io:output_when(fun() ->
-                            io:format("foo~p~n", ["bar"])
-                        end).
+returns_the_function_result_test() ->
+  {result, _} = test_io:redirect("", fun() -> result end).
+
+redirects_the_formatted_output_test() ->
+  {_, "FOO\"BAR\"\n"} =
+    test_io:redirect(
+      "",
+      fun() ->
+          ok = io:format("FOO"),
+          ok = io:format("~p~n", ["BAR"])
+      end).
+
+reads_an_integer_test() ->
+  test_io:redirect(
+    "4",
+    fun() ->
+        {ok, [4]} = io:fread("", "~d")
+    end).
+
+reads_an_integer_in_a_line_test() ->
+  test_io:redirect(
+    "4\n",
+    fun() ->
+        {ok, [4]} = io:fread("", "~d")
+    end).
+
+reads_an_integer_in_the_next_line_test() ->
+  test_io:redirect(
+    "\n4",
+    fun() ->
+        {ok, [4]} = io:fread("", "~d")
+    end).
+
+empty_line_when_reading_integer_errs_test() ->
+  test_io:redirect(
+    "\n",
+    fun() ->
+        {error, {fread, integer}} = io:fread("", "~d")
+    end).
+
+nothing_to_read_test() ->
+  test_io:redirect(
+    "",
+    fun() ->
+        eof = io:fread("", "~d")
+    end).
+
+reads_an_integer_after_an_invalid_one_test() ->
+  test_io:redirect(
+    "foo\n4",
+    fun() ->
+        {error, {fread, integer}} = io:fread("", "~d"),
+        {ok, [4]} = io:fread("", "~d")
+    end).
+
+reads_an_integer_after_an_invalid_one_in_the_second_line_test() ->
+  test_io:redirect(
+    "\nfoo\n4",
+    fun() ->
+        {error, {fread, integer}} = io:fread("", "~d"),
+        {ok, [4]} = io:fread("", "~d")
+    end).
+
+nothing_to_read_after_an_invalid_integer_test() ->
+  test_io:redirect(
+    "foo",
+    fun() ->
+        {error, {fread, integer}} = io:fread("", "~d"),
+        eof = io:fread("", "~d")
+    end).
